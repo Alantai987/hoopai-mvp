f14aeb1d0efe6426c9e50356b6162de1f399f12a
Alantai987 <1540272967@qq.com>
2026-06-22 15:05:01 +0800

    Add daily cards service worker

---
+++ ba-daily-cards/sw.js
@@ -0,0 +1,15 @@
+const CACHE = "ba-daily-cards-v1";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
